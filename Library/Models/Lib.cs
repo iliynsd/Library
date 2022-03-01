1cbbe82d3ca406e4b87c84f0277c09395af5821b
Александр <iliynsd@icloud.com>
2022-03-01 17:33:50 +0300

    add class to keep data

---
+++ Library/Models/Lib.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace Library.Models
+{
+    public class Lib
+    {
+        public List<Book> Books { get; set; }
+        public List<Magazine> Magazines { get; set; }
+    }
+}
