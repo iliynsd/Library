4a644b098e6732b506de6c59127e23bd75723f89
Александр <iliynsd@icloud.com>
2022-03-01 17:34:59 +0300

    finish program

---
+++ Library/Application/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Library.MenuUtils;
+using Library.Models;
+
+namespace Library.Application
+{
+    static class Program
+    {
+        static void Main()
+        {
+            var library = new Lib
+            {
+                Magazines = new List<Magazine>(),
+                Books = new List<Book>()
+            };
+            
+            var cmd = Console.ReadLine();
+            
+            while (cmd != "-end")
+            {
+                 if (cmd == "-add")
+                 {
+                     MenuAddUtil.ShowMenuAdd(library);
+                 }
+                 
+                 if (cmd == "-help")
+                 {
+                     MainMenuUtil.ShowMainMenu();
+                 }
+
+                 if (cmd == "-remove")
+                 {
+                     MenuRemoveUtil.ShowMenuRemove(library);
+                 }
+
+                 if (cmd == "-edit")
+                 {
+                     EditMenuUtil.ShowEditMenu(library);
+                 }
+
+                 if (cmd == "-search")
+                 {
+                     MenuSearchUtil.ShowMenuSearch(library);
+                 }
+                 cmd = Console.ReadLine();
+            }
+        }
+    }
+}
